0acb39b83ba40d7b04b63fecbcbd5098400832ac
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-02-14 19:44:27 +0530

    register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema=new mongoose.Schema({
      },
      watchHistory:[
         {
-            type:Schema.Types.ObjectId,
+            type:mongoose.Schema.Types.ObjectId,
             ref:"Video"
         }
      ],
@@ -52,7 +52,7 @@ const userSchema=new mongoose.Schema({
 )
 userSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
-    this.password=bcrypt.hash(this.password,10)
+    this.password=await bcrypt.hash(this.password,10)
     next()
 })
 userSchema.methods.isPasswordCorrect=async function(password){
