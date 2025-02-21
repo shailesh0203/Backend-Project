4744820cca745212662771678166e49b67f27e3e
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-02-22 00:50:13 +0530

    user-controler update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -195,4 +195,77 @@ const refreshAccessToken=asyncHandler(async(req,res)=>{
   }
 })
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+const changeCurrentPassword=asyncHandler(async(req,res)=>{
+   const {oldPassword,newPassword}=req.body
+
+   const user=await User.findById(req.user?._id)
+   const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+
+   if(!isPasswordCorrect){
+      throw new ApiError(400,"invalid old password")
+   }
+
+   user.password=newPassword
+   await user.save({validateBeforeSave:false})
+
+ return res
+ .status(200)
+ .json(new ApiResponse(200,{},"Password change succesfully"))
+
+
+})
+const getCurrentUser = asyncHandler(async(req, res) => {
+   return res
+   .status(200)
+   .json(new ApiResponse(
+       200,
+       req.user,
+       "User fetched successfully"
+   ))
+})
+
+const updateAccountDetails=asyncHandler(async(req,res)=>{
+   const {fullname,email}=req.body
+
+   if(!fullname || !email){
+      throw new ApiError(400,"All fields are required")
+   }
+
+   const user=User.findByIdAndUpdate(
+      req.user?._id,
+      {
+            $set:{
+               fullname,
+               email:email,
+            }
+      },
+      {new:true}
+   ).select("-password")
+
+   return res
+   .status(200)
+   .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+
+
+const updateUserCoverImage=asyncHandler(async(req,res)=>{
+   const coverImageLocalPath=req.file?.path
+   if(!coverImageLocalPath){
+      throw new ApiError(400,"coverImage file is missing")
+   }
+   const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!coverImage.url){
+      throw new ApiError(400,"Error while uploading coverImage")
+   }
+
+   await User.findByIdAndUpdate(
+      req.user?._id,
+      {$set:{
+         coverImage:coverImage.url
+      }},
+      {new:true}
+   ).select("-password")
+})
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar}
