8aea9be62dd5cdb7ecec90fcac4fcf21f06df250
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-02-25 19:01:26 +0530

    Pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -254,6 +254,11 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
    }
 
    //TODO: delete old image - assignment
+   if (user.avatar) {
+      const oldPublicId = user.avatar.split('/').pop().split('.')[0]; // Extract public ID
+      await cloudinary.uploader.destroy(oldPublicId); // Delete old avatar
+  }
+
 
    const avatar = await uploadOnCloudinary(avatarLocalPath)
 
@@ -305,4 +310,72 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
    )
 })
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage}
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+     const {username}=req.params
+     if(!username?.trim()){
+      return ApiError(400,"username is required")
+     }
+   const channel=await User.aggregate([
+      {
+         $match:{
+            username:username?.toLowerCase()
+         }
+      },
+      {
+         $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"channel",
+            as:"subscribers"
+         }
+      },
+      {
+         $lookup:{
+         from:"subscriptions",
+         localField:"_id",
+         foreignField:"subscriber",
+         as:"subscribedTo"
+         }
+      },
+      {
+         $addFields:{
+            subscribersCount:{
+                  $size:"subscribers"
+            },
+            channelSubscribedToCount:{
+               $size:"subscribedTo"
+            },
+            isSubscribed:{
+               $cond:{
+                  if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                  then:true,
+                  else:false 
+               }
+            }
+         }
+      },
+      {
+         $project:{
+            fullname:1,
+            username:1,
+            subscribersCount:1,
+            channelSubscribedToCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1,
+            email:1
+         }
+      }
+])
+if(!channel?.length){
+   throw new ApiError(404,"Channel does not exist")
+}
+
+return res
+.status(200)
+.json(
+   new ApiResponse(200,channel[0],"user channel fetched succesfully")
+)
+})
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}
